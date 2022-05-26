14dac5642377e327b53608cdc4cbd4e8612ff25a
jastek69 <89563369+jastek69@users.noreply.github.com>
2022-05-26 02:52:05 -0700

    Initial commit

---
+++ build/precache-manifest.9aa72060f3b0a99094ab2ad0214d5abf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "db59d01c8f280b1e5105",
+    "url": "./static/css/main.1d64bd93.chunk.css"
+  },
+  {
+    "revision": "db59d01c8f280b1e5105",
+    "url": "./static/js/main.db59d01c.chunk.js"
+  },
+  {
+    "revision": "660714a3bd665fe7d206",
+    "url": "./static/css/1.4dc392c0.chunk.css"
+  },
+  {
+    "revision": "660714a3bd665fe7d206",
+    "url": "./static/js/1.660714a3.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "280c8ade76ae394e0d5efdb80c1f12a6",
+    "url": "./index.html"
+  }
+];
